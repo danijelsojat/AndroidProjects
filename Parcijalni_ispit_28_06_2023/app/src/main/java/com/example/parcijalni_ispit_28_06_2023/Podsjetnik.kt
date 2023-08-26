7ebfe555c57d70c46664ca6940fb0f5e3d7434f1
Danijel <danijel.sojat@gmail.com>
2023-08-26 15:20:21 +0200

    transfered 2 projects to another folder

--- Parcijalni_ispit_28_06_2023/app/src/main/java/com/example/parcijalni_ispit_28_06_2023/Podsjetnik.kt
+++
@@ -1,3 +0,0 @@
-package com.example.parcijalni_ispit_28_06_2023
-
-data class Podsjetnik(val podsjetnik: String, val detalji: String)
--- Vjezba_15_05/app/src/main/java/com/example/vjezba_15_05/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.vjezba_15_05
-
-data class User(val name: String, val date: String)
